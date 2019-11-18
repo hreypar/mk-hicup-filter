ef1a42db82e7721a0d3e6d0f185006d9bc27b6cd
HRG <hreyes.gopar@gmail.com>
2019-11-17 19:01:29 -0600

    add variables to be used by hicup filter

---
+++ config.mk
@@ -0,0 +1,13 @@
+Configuration variables to run hicup truncator
+#
+# number of CPUs to use
+HF_THREADS_NUMBER = 3
+
+# digested genome path
+DIGESTED_GENOME_PATH =
+
+# maximum allowable insert size (bp)
+LONGEST_NT = 800
+
+# minimum allowable insert size (bp)
+SHORTEST_NT = 150
